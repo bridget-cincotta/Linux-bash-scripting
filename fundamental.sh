ffca810ad5b202eef394eef6f02fdf470c004da5
Marvin-Saputra-SIJA <marvinputra414@gmail.com>
2022-05-29 01:23:27 +0700

    membuat fundamental shell scripting

---
+++ fundamental.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/bash
+
+#how to print a text 
+
+echo "hello world";
+
+#how to defined variables 
+
+name="marvin";
+jurusan="sija";
+
+echo "hallo $name, wellcome to $jurusan";
+
+#Arithmetic Expression 
+
+#sum=$((10+5));
+
+#echo "$sum"
+
+#user input 
+echo "sum calculation program"
+
+echo "please input number: "; read a 
+echo "please input another number: "; read b
+
+sum=$((a+b));
+
+echo "the result of summation is $sum";
